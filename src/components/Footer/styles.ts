323fc2c727b37b5111c2e03c90b70a67d31bf9c3
Igor <ygorponttes@hotmail.com>
2022-09-24 14:31:50 -0300

    aidiconado footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+export const Container = styled.section`
+    
+    div{
+        margin-top: 3em;
+        height: 22.37em;
+        background-color: #858080;
+
+        hr{
+            color: grey;
+            position: relative;
+            top: 5em;
+        }
+        img{
+            height: 3.37em;
+            width: 10.25em;
+            margin: 4em 0 0 40em;
+        }
+
+        span{
+             text-align: center;
+             position: relative;
+             top: 2em;
+             right: 6.5em;
+        }
+        ul{
+            margin-left: 5em; 
+            display: flex;
+            justify-content: center;
+
+            li{
+                padding: 0 10em 0 0;
+                position: relative;
+                top: 9em;
+            }
+        }
+    }
+
+`;
