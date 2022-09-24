668384afb44f05a1eb273dfc25dfd7d19f1b45ef
Oliverio <oliverio.junior2@gmail.com>
2022-09-24 11:18:53 -0300

    Ajustar SingIn

--- src/pages/LoginOrSingIn/index.tsx
+++ src/pages/LoginOrSingIn/index.tsx
@@ -1,14 +1,48 @@
-import { Container, Form, Logo } from './styles';
+import { Container, ContainerInput, Form, Logo, WrapperLogo } from './styles';
 import logo from '../../assets/logo.png';
 export const LoginOrSingIn: React.FC = () => {
+  const form = [
+    {
+      id: 1,
+      placeholder: 'nome',
+      min: 3,
+    },
+    {
+      id: 2,
+      placeholder: 'email',
+      min: 3,
+    },
+    {
+      id: 3,
+      placeholder: 'senha',
+      min: 6,
+    },
+    {
+      id: 4,
+      placeholder: 'confirmar-senha',
+      min: 6,
+    },
+  ];
   return (
     <Container>
-      <Logo>
-        <h1>G P A</h1>
+      <WrapperLogo>
+        <Logo>GPA</Logo>
         <img src={logo} alt="logo" />
-      </Logo>
+      </WrapperLogo>
 
-      <Form>Login</Form>
+      <Form>
+        {form.map(item => {
+          return (
+            <ContainerInput key={item.id}>
+              <input
+                type="text"
+                min={item.min}
+                placeholder={item.placeholder}
+              />
+            </ContainerInput>
+          );
+        })}
+      </Form>
     </Container>
   );
 };
--- src/pages/LoginOrSingIn/styles.ts
+++ src/pages/LoginOrSingIn/styles.ts
@@ -10,18 +10,53 @@ export const Container = styled.div`
 `;
 
 export const Form = styled.form`
-  width: 200px;
+  width: 300px;
   height: 400px;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #4132c1;
+  border-radius: 0.5rem;
+`;
+export const WrapperLogo = styled.div`
+  width: 164px;
+  height: 54px;
+  display: flex;
+  img {
+    border-bottom-right-radius: 0.5rem;
+    border-bottom-left-radius: 0.5rem;
+  }
+`;
+export const Logo = styled.h1`
+  width: 164px;
+  height: 54px;
+  position: absolute;
+  border-radius: 0.5rem;
+  opacity: 0.5;
+  filter: drop-shadow(1px 1px 2px #0033ff);
+  font-weight: 500;
+  text-align: center;
 `;
 
-export const Logo = styled.div`
-  width: 164px;
-  height: 59px;
-  background-color: pink;
-  h1 {
-    position: relative;
-    top: 55px;
-    left: 35px;
-    opacity: 0.5;
+export const ContainerInput = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  &:nth-child(n + 2) {
+    margin-top: 2rem;
+  }
+  input {
+    width: 80%;
+    height: 30px;
+    border: none;
+    padding-left: 5px;
+    border-radius: 4px;
+    &:focus {
+      outline: none;
+    }
   }
 `;
